02775558bc0396bd62db9b0cddfe3c9fc80b8a77
Emre Karadeniz <emrekrdenz@gmail.com>
2022-06-12 22:25:15 +0300

    enuygun test case

---
+++ src/test/java/org/enuygun/Base/BaseTest.java
@@ -0,0 +1,110 @@
+package org.enuygun.Base;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.gauge.AfterScenario;
+import com.thoughtworks.gauge.BeforeScenario;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.enuygun.model.ElementInfo;
+import org.enuygun.step.AylarConst;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class BaseTest {
+
+    public static WebDriver driver;
+    String browser="chrome";
+    private static final String DEFAULT_DIRECTORY_PATH = "elementValues";
+    ConcurrentMap<String, Object> elementMapList = new ConcurrentHashMap<>();
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+
+
+    @BeforeScenario
+    public void setUp(){
+
+        if (driver == null) {
+
+            if("chrome".equalsIgnoreCase(browser)) {
+
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--disable-notifications");
+                chromeOptions.addArguments("--kiosk");
+                driver = new ChromeDriver(chromeOptions);
+
+            }
+
+            if("firefox".equalsIgnoreCase(browser)) {
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--disable-notifications");
+                firefoxOptions.addArguments("--kiosk");
+                driver = new FirefoxDriver(firefoxOptions);
+            }
+        }
+        driver.manage().window().maximize();
+        AylarConst.addErrorCodeConsts();
+    }
+
+    @AfterScenario
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    public void initMap(File[] fileList) {
+        Type elementType = new TypeToken<List<ElementInfo>>() {
+        }.getType();
+        Gson gson = new Gson();
+        List<ElementInfo> elementInfoList = null;
+        for (File file : fileList) {
+            try {
+                elementInfoList = gson
+                        .fromJson(new FileReader(file), elementType);
+                elementInfoList.parallelStream()
+                        .forEach(elementInfo -> elementMapList.put(elementInfo.getKey(), elementInfo));
+            } catch (FileNotFoundException e) {
+                logger.warn("{} not found", e);
+            }
+        }
+    }
+    public File[] getFileList() {
+        File[] fileList = new File(
+                this.getClass().getClassLoader().getResource(DEFAULT_DIRECTORY_PATH).getFile())
+                .listFiles(pathname -> !pathname.isDirectory() && pathname.getName().endsWith(".json"));
+        if (fileList == null) {
+            logger.warn(
+                    "File Directory Is Not Found! Please Check Directory Location. Default Directory Path = {}",
+                    DEFAULT_DIRECTORY_PATH);
+            throw new NullPointerException();
+        }
+        return fileList;
+    }
+
+    public ElementInfo findElementInfoByKey(String key) {
+        return (ElementInfo) elementMapList.get(key);
+    }
+
+    public void saveValue(String key, String value) {
+        elementMapList.put(key, value);
+    }
+
+    public String getValue(String key) {
+        return elementMapList.get(key).toString();
+    }
+
+}
---
+++ src/test/java/org/enuygun/step/BaseSteps.java
@@ -0,0 +1,180 @@
+package org.enuygun.step;
+
+import com.thoughtworks.gauge.Gauge;
+import com.thoughtworks.gauge.Step;
+import org.enuygun.Base.BaseTest;
+import org.enuygun.model.ElementInfo;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BaseSteps extends BaseTest {
+
+    public BaseSteps() {
+        initMap(getFileList());
+    }
+
+    private void clickElement(WebElement element) {
+        element.click();
+    }
+
+    WebElement findElement(String key) {
+        By infoParam = getElementInfoToBy(findElementInfoByKey(key));
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 30);
+        WebElement webElement = webDriverWait
+                .until(ExpectedConditions.presenceOfElementLocated(infoParam));
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView({behavior: 'smooth', block: 'center', inline: 'center'})",
+                webElement);
+        return webElement;
+    }
+
+    List<WebElement> findElements(String key) {
+        return driver.findElements(getElementInfoToBy(findElementInfoByKey(key)));
+    }
+
+    public By getElementInfoToBy(ElementInfo elementInfo) {
+        By by = null;
+        if (elementInfo.getType().equals("css")) {
+            by = By.cssSelector(elementInfo.getValue());
+        } else if (elementInfo.getType().equals(("name"))) {
+            by = By.name(elementInfo.getValue());
+        } else if (elementInfo.getType().equals("id")) {
+            by = By.id(elementInfo.getValue());
+        } else if (elementInfo.getType().equals("xpath")) {
+            by = By.xpath(elementInfo.getValue());
+        } else if (elementInfo.getType().equals("linkText")) {
+            by = By.linkText(elementInfo.getValue());
+        } else if (elementInfo.getType().equals(("partialLinkText"))) {
+            by = By.partialLinkText(elementInfo.getValue());
+        }
+        return by;
+    }
+
+    @Step("<key> elementine <text> textini yaz")
+    public void ssendKeys(String key, String text) {
+        if (!key.equals("")) {
+            findElement(key).sendKeys(text);
+            logger.info(key + " elementine " + text + " texti yazildi.");
+        }
+    }
+
+    @Step("<key> Elementine tıkla")
+    public void clickElement(String key) {
+        if (!key.isEmpty()) {
+            //hoverElement(findElement(key));
+            clickElement(findElement(key));
+            logger.info(key + " elementine tiklandi.");
+        }
+    }
+
+    @Step("<url> adresine git")
+    public void goToUrl(String url){
+        driver.navigate().to(url);
+        Gauge.writeMessage("Page title is %s",driver.getTitle());
+        Gauge.captureScreenshot();
+    }
+
+    @Step("<saniye> saniye bekle")
+    public void bekle(int saniye) throws InterruptedException {
+        Thread.sleep(saniye*1000);
+    }
+
+
+    @Step("<key> li checkbox secili degilse sec")
+    public void checkbox(String key){
+        boolean isMarkedDone = findElement(key).isSelected();
+        System.out.println("Tek yon secili." +isMarkedDone);
+
+        if(isMarkedDone==true){
+           clickElement(key);
+            System.out.println("Gidis Dönüs olarak secildi.");
+        }
+
+    }
+
+    @Step("ddMMyy formatında <day>-<month>-<year> bilet tarihi giriniz.")
+    public static void selectDate2(String day,String month,int year) throws InterruptedException {
+
+        LocalDate localDate=LocalDate.now();
+        int localYear=localDate.getYear();
+        String ayYil=AylarConst.getAy(month)+" "+year;
+
+
+        if(localYear==year || localYear==((year)-1)){
+
+            while(true){
+                String text=driver.findElement(By.xpath("//div[@data-visible='true']//div[@class='CalendarMonth_caption CalendarMonth_caption_1']")).getText();
+
+                if(text.equals(ayYil)){
+                    break;
+                }
+                else{
+                    driver.findElement(By.xpath("//div[@role='button' and @aria-label='Move forward to switch to the next month.']")).click();
+                    Thread.sleep(1000);
+                }
+            }
+
+            List <WebElement> allDates=driver.findElements(By.xpath("//div[@data-visible='true']/table/tbody/tr/td[@role='button' and @aria-disabled='false' and @aria-label=contains(@aria-label,'"+AylarConst.getAy(month)+"')]"));
+
+            for(WebElement ele:allDates)
+            {
+                String text=ele.getText();
+                if(text.equals(day)){
+                    ele.click();
+                    System.out.println("Elemente tiklandi..");
+                    break;
+                }
+            }
+        }
+        else {
+            Assertions.fail("Lutfen gecerli bir tarih araligi giriniz!.. Girilebilecek Yıllar : "+localYear+ "ve "+((localYear)+1));
+        }
+    }
+
+    @Step("<key> elementini kontrol et")
+    public void checkElement(String key) {
+        assertTrue(findElement(key).isDisplayed(), "Aranan element bulunamadi");
+        logger.info(key+ "elementi gorundu.");
+    }
+
+    @Step("<key> yolcusunun biletini <adet> adet arttır")
+    public void yolcu(String key,int adet){
+
+        if(adet>9){
+            for(int i=0; i<adet;i++){
+                clickElement(key);
+            }
+            checkElement("grupUcusTeklifiAl");
+            logger.info("Grup Ucus Teklifi Al Butonu Gorundu");
+        }
+        else{
+            for(int i=0; i<adet;i++){
+                clickElement(key);
+            }
+        }
+        clickElement("yolcuTamamButonu");
+    }
+
+    public void javascriptclicker(WebElement element) {
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].click();", element);
+    }
+
+    @Step("<key> elementine javascript ile tıkla")
+    public void clickToElementWithJavaScript(String key) {
+        WebElement element = findElement(key);
+        javascriptclicker(element);
+        logger.info(key + " elementine javascript ile tiklandi");
+    }
+
+
+}
